70a42f6f489fa257e3110d255c66a6dbe248cc1d
André Oliveira <andremoreiraoliveira2000@gmail.com>
2023-10-26 00:19:28 +0100

    update requirements and install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # install multi-object tracker
 conda activate hybrik
-pip install -e git+https://github.com/mkocabas/yolov3-pytorch.git#egg=yolov3
-pip install -e git+https://github.com/haofanwang/multi-person-tracker.git#egg=multi-person-tracker
-pip install numba filterpy
-conda activate glamr
+sudo pip install -e git+https://github.com/mkocabas/yolov3-pytorch.git#egg=yolov3
+sudo pip install -e git+https://github.com/haofanwang/multi-person-tracker.git#egg=multi-person-tracker
+sudo pip install numba filterpy
+# conda activate glamr
 
 # apt
 sudo apt install libgl1-mesa-glx xvfb
@@ -13,3 +13,12 @@ sudo apt update
 sudo wget https://github.com/mmatl/travis_debs/raw/master/xenial/mesa_18.3.3-0.deb
 sudo dpkg -i ./mesa_18.3.3-0.deb || true
 sudo apt install -f
+
+
+conda activate mot
+cd src/yolo_tracking
+sudo pip install -v -e .
+
+sudo pip install boxmot
+
+cd ../..
