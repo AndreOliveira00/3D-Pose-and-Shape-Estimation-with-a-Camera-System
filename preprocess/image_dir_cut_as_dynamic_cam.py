35b676d27e31bfe011c1895ef691401caddac02f
André Oliveira <andremoreiraoliveira2000@gmail.com>
2023-10-26 23:26:52 +0100

    update requirements and install.sh

--- preprocess/image_dir_cut_as_dynamic_cam.py
+++ preprocess/image_dir_cut_as_dynamic_cam.py
@@ -20,52 +20,52 @@ def verificar_mesma_largura(diretorio, largura_referencia, jump):
             imagem = Image.open(caminho_arquivo)
             # print(imagem.width)
             if imagem.width != largura_referencia:
-                print(f"As imagens não têm a mesma largura. -> {os.path.basename(caminho_arquivo)}")
+                print(f"As imagens não têm a mesma width. -> {os.path.basename(caminho_arquivo)}")
                 return
 
-    print("Todas as imagens têm a mesma largura.")
+    print("Todas as imagens têm a mesma width.")
 
-def cut_images(diretorio_origem, x_init, y_init, largura, altura, diretorio_destino=None):
-    if diretorio_destino is None:
-        diretorio_destino=diretorio_origem
-    if not os.path.exists(diretorio_destino):
-        os.makedirs(diretorio_destino)
+def cut_images(source_directory, x_init, y_init, width, height, destination_directory=None):
+    if destination_directory is None:
+        destination_directory=source_directory
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
     
-    for arquivo in tqdm(sorted(os.listdir(diretorio_origem))):
-        caminho_arquivo_origem = os.path.join(diretorio_origem, arquivo)
+    for arquivo in tqdm(sorted(os.listdir(source_directory))):
+        caminho_arquivo_origem = os.path.join(source_directory, arquivo)
         if os.path.isfile(caminho_arquivo_origem):
             imagem = Image.open(caminho_arquivo_origem)
             largura_imagem, altura_imagem = imagem.size
-            x_final = min(x + largura, largura_imagem)
-            y_final = min(y + altura, altura_imagem)
+            x_final = min(x + width, largura_imagem)
+            y_final = min(y + height, altura_imagem)
             cut_image = imagem.crop((x, y, x_final, y_final))
-            nome_arquivo_destino = os.path.join(diretorio_destino, arquivo)
+            nome_arquivo_destino = os.path.join(destination_directory, arquivo)
             cut_image.save(nome_arquivo_destino)
             
-def cut_images_as_dyn(diretorio_origem, diretorio_destino, x, y, largura, altura):
-    if not os.path.exists(diretorio_destino):
-        os.makedirs(diretorio_destino)
+def cut_images_as_dyn(source_directory, destination_directory, x, y, width, height):
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
     flag_up = True
 
-    for arquivo in tqdm(sorted(os.listdir(diretorio_origem))):
-        caminho_arquivo_origem = os.path.join(diretorio_origem, arquivo)
+    for arquivo in tqdm(sorted(os.listdir(source_directory))):
+        caminho_arquivo_origem = os.path.join(source_directory, arquivo)
         if os.path.isfile(caminho_arquivo_origem):
             imagem = Image.open(caminho_arquivo_origem)
             largura_imagem, altura_imagem = imagem.size
 
-            x_final = min(x + largura, largura_imagem)
-            y_final = min(y + altura, altura_imagem)
+            x_final = min(x + width, largura_imagem)
+            y_final = min(y + height, altura_imagem)
 
             regiao_cortada = imagem.crop((x, y, x_final, y_final))
 
-            nome_arquivo_destino = os.path.join(diretorio_destino, arquivo)
+            nome_arquivo_destino = os.path.join(destination_directory, arquivo)
             regiao_cortada.save(nome_arquivo_destino)
 
             # Atualiza o valor de x para a próxima iteração
-            if flag_up and x + largura < largura_imagem:
-                x += jump             # largura / jump deve ser interior senão imagens ficam com diferentes resoluções
+            if flag_up and x + width < largura_imagem:
+                x += jump             # width / jump deve ser interior senão imagens ficam com diferentes resoluções
             else:
-                if x + largura > largura+60:
+                if x + width > width+60:
                     x -= jump
                     flag_up = False
                 else:
@@ -74,37 +74,37 @@ def cut_images_as_dyn(diretorio_origem, diretorio_destino, x, y, largura, altura
 
 
 # Diretório das imagens originais
-diretorio_origem = 'assets/dynamic/courtyard_basketball_01_0_469'
+source_directory = 'assets/dynamic/courtyard_basketball_01_0_469'
 
 # Coordenadas de início do corte (ponto x, y)
 x = 320
 y = 240
 
-# Dimensões do corte (largura, altura)
-largura = 516 # must be divisible by 2
-altura = 1350
+# Dimensões do corte (width, height)
+width = 516 # must be divisible by 2
+height = 1350
 
-# largura / jump deve ser interior senão imagens ficam com diferentes resoluções
+# width / jump deve ser interior senão imagens ficam com diferentes resoluções
 jump=12
 
 # Diretório de destino para as imagens cortadas
-diretorio_destino = f'{diretorio_origem}/cut_images_{largura}x{altura}'
+destination_directory = f'{source_directory}/cut_images_{width}x{height}'
 
 # Chama a função para cortar as imagens
-cut_images_as_dyn(diretorio_origem, diretorio_destino, x, y, largura, altura)
+cut_images_as_dyn(source_directory, destination_directory, x, y, width, height)
 
-verificar_mesma_largura(diretorio_destino, largura, jump)
+verificar_mesma_largura(destination_directory, width, jump)
 
 
 # x = 560
 # y = 0
-# largura = 1360 # must be divisible by 2
-# altura = 1010
-# diretorio_origem = "out/glamr_static/WalkingAround_Joe_1s/pose_est_hybrik_hrnet48/frames"
-# diretorio_destino = "assets/static/WalkingAround_Joe_0_10"
-# cut_images(diretorio_origem,x,y,largura,altura,diretorio_destino)
+# width = 1360 # must be divisible by 2
+# height = 1010
+# source_directory = "out/glamr_static/WalkingAround_Joe_1s/pose_est_hybrik_hrnet48/frames"
+# destination_directory = "assets/static/WalkingAround_Joe_0_10"
+# cut_images(source_directory,x,y,width,height,destination_directory)
 
-# frame_dir = diretorio_destino
+# frame_dir = destination_directory
 # vid_out_file =  "assets/static/WalkingAround_Joe_0_10.mp4"
 # init_frame = 1
 # images_to_video(frame_dir, vid_out_file, init_frame, fps=30, verbose=False)
